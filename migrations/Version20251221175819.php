2c5d59ea331a894798f0bc498222d3488ae0436a
Clément <33451311+clmntgbr@users.noreply.github.com>
2025-12-21 18:59:23 +0100

    Update environment configuration and add database migration for clip and user entities
    This commit renames the RabbitMQ password variable in the `.env.dist` file for clarity. Additionally, it introduces a new migration file that creates the `clip`, `user`, and `video` tables, along with their respective constraints and indexes, enhancing the database schema for better data management.

---
+++ migrations/Version20251221175819.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251221175819 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE clip (thumbnail VARCHAR(255) DEFAULT NULL, status VARCHAR(255) NOT NULL, statuses JSON NOT NULL, id UUID NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, original_video_id UUID NOT NULL, user_id UUID NOT NULL, PRIMARY KEY (id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_AD201467A9EE75F ON clip (original_video_id)');
+        $this->addSql('CREATE INDEX IDX_AD201467A76ED395 ON clip (user_id)');
+        $this->addSql('CREATE TABLE "user" (email VARCHAR(180) NOT NULL, firstname VARCHAR(255) DEFAULT NULL, lastname VARCHAR(255) DEFAULT NULL, picture VARCHAR(255) DEFAULT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, id UUID NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY (id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON "user" (email)');
+        $this->addSql('CREATE TABLE video (name VARCHAR(255) DEFAULT NULL, original_name VARCHAR(255) NOT NULL, url TEXT DEFAULT NULL, duration INT DEFAULT NULL, size INT DEFAULT NULL, format VARCHAR(255) DEFAULT NULL, audio_files JSON DEFAULT NULL, id UUID NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY (id))');
+        $this->addSql('ALTER TABLE clip ADD CONSTRAINT FK_AD201467A9EE75F FOREIGN KEY (original_video_id) REFERENCES video (id) ON DELETE CASCADE NOT DEFERRABLE');
+        $this->addSql('ALTER TABLE clip ADD CONSTRAINT FK_AD201467A76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE NOT DEFERRABLE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE clip DROP CONSTRAINT FK_AD201467A9EE75F');
+        $this->addSql('ALTER TABLE clip DROP CONSTRAINT FK_AD201467A76ED395');
+        $this->addSql('DROP TABLE clip');
+        $this->addSql('DROP TABLE "user"');
+        $this->addSql('DROP TABLE video');
+    }
+}
